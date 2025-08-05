99c1bc2c6044b6d4b8be4372c0a6c9e91da0f41b
grscheller <geoffrey@scheller.com>
2025-08-05 07:53:00 -0600

    First steps to remove from "__future__ import annotations"

--- tests/test_splitend.py
+++ tests/test_splitend.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
 from pythonic_fp.splitends.splitend import SplitEnd as SE
 from pythonic_fp.iterables.merging import concat
 
--- tests/test_splitend_node.py
+++ tests/test_splitend_node.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
 from pythonic_fp.fptools.maybe import MayBe
 from pythonic_fp.splitends.splitend_node import SENode
 
