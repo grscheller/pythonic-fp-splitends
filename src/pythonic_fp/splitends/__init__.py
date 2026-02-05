25630f808e3b8fff68f878744beda843b27da4c5
grscheller <geoffrey@scheller.com>
2026-02-05 13:52:17 -0500

    Finished effort homogenizing pythonic-fp docs

--- src/pythonic_fp/splitends/__init__.py
+++ src/pythonic_fp/splitends/__init__.py
@@ -12,24 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Mutable stack objects that can safely share data.
+"""
+.. admonition:: Mutable stack objects that can safely share data
 
-Python package implementing a singularly linked stack of nodes called a ``SplitEnd``.
-While mutable, these data structures can safely share immutable data nodes between
-themselves. These nodes, of type ``SENode``, always contain data. A root node is
-a node whose previous node is itself. Root nodes mark the bottom of the stack.
+    Python package implementing a singularly linked stack of nodes called a ``SplitEnd``.
+    While mutable, these data structures can safely share immutable data nodes between
+    themselves. These nodes, of type ``SENode``, always contain data. A root node is
+    a node whose previous node is itself. Root nodes mark the bottom of the stack.
 
-Like one of many "split ends" from shafts of hair, a ``SplitEnd`` can be "snipped"
-shorter or "extended" further from its "end" node at the top of the stack. Its "root"
-node is at the bottom of the stack and can not be "snipped" or "cut" off.
+    Like one of many "split ends" from shafts of hair, a ``SplitEnd`` can be "snipped"
+    shorter or "extended" further from its "end" node at the top of the stack. Its "root"
+    node is at the bottom of the stack and can not be "snipped" or "cut" off.
 
-A ``SplitEnd`` can be duplicated onto a compatible root node. By compatible, the
-new root must contain data that compares as equal to the old root node.
+    A ``SplitEnd`` can be duplicated onto a compatible root node. By compatible, the
+    new root must contain data that compares as equal to the old root node.
 
-**TODO:** A ``scalp`` is a container of root nodes all of whose contained data must
-be hashable. The ``scalp.add(d)`` method will return the unique root node with data
-that compares as equal to ``d``, or create a new root node if such a contained node
-does not exist.
+.. admonition:: Future directions
+
+    A ``scalp`` is a container of root nodes all of whose contained data must
+    be hashable. The ``scalp.add(d)`` method will return the unique root node with data
+    that compares as equal to ``d``, or create a new root node if such a contained node
+    does not exist.
 
 """
 
